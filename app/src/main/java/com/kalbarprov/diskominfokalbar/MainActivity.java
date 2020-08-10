507624888d2cdb5bb139a4ca7a641dde80193834
galaxyan12 <jody@student.untan.ac.id>
2020-08-10 13:51:44 +0700

    second layout

--- app/src/main/java/com/kalbarprov/diskominfokalbar/MainActivity.java
+++ app/src/main/java/com/kalbarprov/diskominfokalbar/MainActivity.java
@@ -2,15 +2,31 @@ package com.kalbarprov.diskominfokalbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgStruktur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgStruktur = findViewById(R.id.struktur_organisasi);
+        imgStruktur.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StrukturActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
